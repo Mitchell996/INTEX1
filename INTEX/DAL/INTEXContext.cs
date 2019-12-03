5a068b8788c45f5e9783ba6c8d2e3cef8d14583e
MItchell Johnson <mjohns46@byu.edu>
2019-12-03 13:22:38 -0700

    Added in DAL and database stuff

---
+++ INTEX/DAL/INTEXContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using INTEX.Models;
+
+namespace INTEX.DAL
+{
+    public class INTEXContext : DbContext
+    {
+        public INTEXContext() : base("INTEX")
+          {
+          }
+        public DbSet<Assay> Assay { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Materials> Material { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderTest> OrderTest { get; set; }
+        public DbSet<Sample> Sample { get; set; }
+        public DbSet<Test> Test { get; set; }
+        public DbSet<TestMaterials> TestMaterial { get; set; }
+        public DbSet<TestMeasurement> TestMeasurement { get; set; }
+
+
+    }
+}
--- INTEX/Global.asax.cs
+++ INTEX/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using INTEX.Models;
+using INTEX.DAL;
+using System.Data.Entity;
 
 namespace INTEX
 {
@@ -12,6 +15,8 @@ namespace INTEX
     {
         protected void Application_Start()
         {
+            Database.SetInitializer<INTEXContext>(null);
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
